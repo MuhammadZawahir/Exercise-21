55cc330a0d3c838619ad048728656ddb48a0816b
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:38:34 +0500

    Exercise 21

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.*/
+let country1 = {
+    name: "India",
+    capital: "New Delhi",
+    population: 1300000000,
+    area: 300000,
+    continent: "Asia"
+};
+let country2 = {
+    name: "China",
+    capital: "Beijing",
+    population: 1000000000,
+    area: 800000,
+    continent: "Asia"
+};
+let country3 = {
+    name: "USA",
+    capital: "Washington",
+    population: 300000000,
+    area: 100000,
+    continent: "North America"
+};
+console.log("Country1:");
+console.log("name:", country1.name);
+console.log("capital:", country1.capital);
+console.log("population:", country1.population);
+console.log("area:", country1.area);
+console.log("continent:", country1.continent);
+console.log();
+console.log("Country2:");
+console.log("name:", country2.name);
+console.log("capital:", country2.capital);
+console.log("population:", country2.population);
+console.log("area:", country2.area);
+console.log("continent:", country2.continent);
+console.log();
+console.log("Country3:");
+console.log("name:", country3.name);
+console.log("capital:", country3.capital);
+console.log("population:", country3.population);
+console.log("area:", country3.area);
+console.log("continent:", country3.continent);
